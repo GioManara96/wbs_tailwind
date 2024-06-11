6f300492ba54b351b63d185006407d4329879804
Giovanni <g.manara@websonica.net>
2024-06-11 14:13:58 +0200

    aggiunto hero section con immagine di background e CTA

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,20 @@ function App() {
     <>
       <Header />
       <main>
-        <h1>Hello World</h1>
+        {/** hero section */}
+        <div className="w-full min-h-[700px] bg-hero-pattern bg-center bg-scroll bg-no-repeat bg-cover flex justify-center items-center relative">
+          {/** overlay */}
+          <div className="hero-overlay"></div>
+          {/** content */}
+          <div className="relative">
+            <a
+              href="#"
+              title="cta da qualche parte"
+              className="bg-esa-blue text-white px-4 py-2 uppercase font-serif text-2xl border border-esa-blue rounded font-semibold hover:bg-white hover:text-esa-blue mb-5 transition ease-in-out duration-300">
+              prova
+            </a>
+          </div>
+        </div>
       </main>
       <Footer />
     </>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "hero-pattern": "url('https://www.esaspaceshop.com/pub/media/wysiwyg/slider/Banner_ESS_ESA_X_LOQI_1.webp')",
+      },
+      backgroundColor: {
+        "esa-blue": "#003247",
+      },
+      textColor: {
+        "esa-blue": "#003247",
+      },
+      borderColor: {
+        "esa-blue": "#003247",
+      },
+    },
   },
   plugins: [],
-}
+};
