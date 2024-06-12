be485b1f94a1e5b220f58fa58af7630839977e5c
Giovanni <g.manara@websonica.net>
2024-06-12 12:10:16 +0200

    aggiunta griglia fatta con display grid

--- src/App.jsx
+++ src/App.jsx
@@ -20,8 +20,24 @@ function App() {
             </a>
           </div>
         </div>
+
+        {/** sezione a griglia */}
+        <section className="my-10 w-full">
+          <div className="mx-auto w-1/2 min-h-[400px] grid grid-cols-4 grid-rows-4 gap-5">
+            {/** colonna sinistra in alto */}
+            <div className="col-span-2 md:col-span-1 row-span-2 bg-gray-200 order-1">1</div>
+            {/** colonna centrale */}
+            <div className="col-span-4 md:col-span-2 row-span-4 bg-red-400 order-3 md:order-2">2</div>
+            {/** colonna destra in alto */}
+            <div className="col-span-2 md:col-span-1 row-span-2 bg-green-300 order-2 md:order-3">3</div>
+            {/** colonna sinistra in basso */}
+            <div className="col-span-2 md:col-span-1 row-span-2 bg-yellow-300 order-4">4</div>
+            {/** colonna destra in basso */}
+            <div className="col-span-2 md:col-span-1 row-span-2 bg-orange-300 order-5">5</div>
+          </div>
+        </section>
       </main>
-      <Footer />
+      {/* <Footer /> */}
     </>
   );
 }
